f498490cb52b8b3413c54f37606a8b8645011487
Pbrv <pilarbravo.17@gmail.com>
2024-03-22 13:44:04 +0100

    excepciones en routers

--- APIREST/src/routers/usuarios.py
+++ APIREST/src/routers/usuarios.py
@@ -3,6 +3,8 @@ from typing import Annotated
 from models import usuario
 from database.database import SessionLocal
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from pydantic import ValidationError
 
 app = APIRouter()
 
@@ -20,18 +22,27 @@ db_con = Annotated[Session,Depends(get_db)]
 #METODO DE PRUEBA
 @app.get("/mostrar_usuarios")
 async def recetas_mostrar(db:db_con):
-    usuarios = db.query(usuario.Usuario).all()
-    #Asi seria la consulta con "where" siempre al final hay que añadir "first" si solo quieres una fila si no "all"
-    # recetas = db.query(receta.Receta).where(receta.Receta.dificultad_receta == 1).first()
-    # recetas = db.query(receta.Receta).where(receta.Receta.dificultad_receta == 1).all()
-    return usuarios
+    try:
+        usuarios = db.query(usuario.Usuario).all()
+        #Asi seria la consulta con "where" siempre al final hay que añadir "first" si solo quieres una fila si no "all"
+        # recetas = db.query(receta.Receta).where(receta.Receta.dificultad_receta == 1).first()
+        # recetas = db.query(receta.Receta).where(receta.Receta.dificultad_receta == 1).all()
+        return usuarios
+    except SQLAlchemyError as se:
+        raise HTTPException(status_code=500, detail=f"Error en la base de datos: {se}")
+
 
 @app.post("/registrar_usuario")
 async def recetas_mostrar(insertar: usuario.RegistrarUsuario,db:db_con):
-    informacion_usuario = insertar.dict()
-    informacion_usuario['suscripcion_usuario'] = 1 #Crearle de base la suscripcion gratuita
-     # Se podria usar **insertar.dict si no queremos modificar o añadir ningun campo a lo que hemos pedido al usuario para que inserte
-    usuario_insertar = usuario.Usuario(**informacion_usuario)
-    db.add(usuario_insertar)
-    db.commit()
-    return "Usuario registrado"
+    try:
+        informacion_usuario = insertar.dict()
+        informacion_usuario['suscripcion_usuario'] = 1 #Crearle de base la suscripcion gratuita
+        # Se podria usar **insertar.dict si no queremos modificar o añadir ningun campo a lo que hemos pedido al usuario para que inserte
+        usuario_insertar = usuario.Usuario(**informacion_usuario)
+        db.add(usuario_insertar)
+        db.commit()
+        return "Usuario registrado correctamente"
+    except ValidationError as ve:
+        raise HTTPException(status_code=422, detail=f"Validación fallida: {ve}")
+    except SQLAlchemyError as se:
+        raise HTTPException(status_code=500, detail=f"Error en la base de datos: {se}")

ca9cec3bd0659609c8a097f773c3036e58228d07
Pbrv <pilarbravo.17@gmail.com>
2024-06-08 14:07:41 +0200

    filtros sin terminar

--- app/src/componentes/Recetas.jsx
+++ app/src/componentes/Recetas.jsx
@@ -9,10 +9,12 @@ function Recetas (){
     const [filtros, setFiltros] = useState('');
     const [pagina, setPagina] = useState(1); //Paginacion de las recetas
     const filtrosDisponibles = ['Salsa','Patata','Queso','Lechuga','Arroz'];
+    const [mostrarDificultad, setMostrarDificultad] = useState(false); // Filtro de dificultad
+    let valorFiltro;
 
     // Hacer que se muestren todas las recetas si no hay filtros
     useEffect(() => {
-        fetch(`http://localhost:8000/recetas_filtros/?filtro=${filtros}&pagina=${pagina}`)
+        fetch(`http://localhost:8000/recetas_filtros/?filtro=${filtros}&dificultad=${valorFiltro}&pagina=${pagina}`)
             .then(response => response.json())
             .then((recetas) => {
                 if(recetas.length === 0) {
@@ -25,6 +27,26 @@ function Recetas (){
 
     // Funcion para cuando se añada un ingrediente a filtrar o se elimine
     const cambiar_filtros = (value, e) => {
+        let valorFiltro;
+        switch(value) {
+            case 'Muy facil':
+                valorFiltro = 1;
+                break;
+            case 'Facil':
+                valorFiltro = 2;
+                break;
+            case 'Medio':
+                valorFiltro = 3;
+                break;
+            case 'Dificil':
+                valorFiltro = 4;
+                break;
+            case 'Muy dificil':
+                valorFiltro = 5;
+                break;
+            default:
+                valorFiltro = value;
+        }
         if (filtros.includes(';'+value)){
             setFiltros(filtros.replace(';'+ value,''))
         } else if (filtros.includes(value + ';')) {
@@ -42,9 +64,21 @@ function Recetas (){
             <div className="div-recetas-filtros">
                 {/* ID puesto para saber que contiene */}
                 <div id="filtros_recetas">
-                    {filtrosDisponibles.map((filtro, index) => (
-                        <Boton key={index} onClick={(e) => cambiar_filtros(e.target.innerText, e.target)} value={filtro} />
-                    ))}
+                    <h4>Filtra aquí tus resultados</h4>
+                    <div className="div-botones-filtros">
+                    <Boton onClick={() => setMostrarDificultad(!mostrarDificultad)} value="Dificultad" />
+                        {mostrarDificultad && (
+                            <div className="div-botones-dificultad">
+                                {['Muy facil', 'Facil', 'Medio', 'Dificil', 'Muy dificil'].map((dificultad, index) => (
+                                    <Boton key={index} onClick={(e) => cambiar_filtros(e.target.innerText, e.target)} value={dificultad} />
+                                ))}
+                            </div>
+                        )}
+                        {filtrosDisponibles.map((filtro, index) => (
+                            <Boton key={index} onClick={(e) => cambiar_filtros(e.target.innerText, e.target)} value={filtro} />
+                        ))}
+                        
+                    </div>
                 </div>
                 <div className="recetas-destacadas">
                     {recetas.map((receta) => (
